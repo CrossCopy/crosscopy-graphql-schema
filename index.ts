8b296cce7b4cff106e0d5cfce6bb52dec1459d4b
Huakun Shen <huakun.shen@huakunshen.com>
2022-06-24 01:27:35 -0400

    Remove typescript files that require vue or react

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 import * as operations from "./generated/ops";
 import * as requests from "./generated/gql-req";
 import * as docNode from "./generated/doc-node";
-import * as urql from "./generated/urql";
-import * as vueUrql from "./generated/vue-urql";
-import * as vueApollo from "./generated/vue-apollo";
-import * as vueApolloSmartOp from "./generated/vue-apollo-smart-op";
+// import * as urql from "./generated/urql";
+// import * as vueUrql from "./generated/vue-urql";
+// import * as vueApollo from "./generated/vue-apollo";
+// import * as vueApolloSmartOp from "./generated/vue-apollo-smart-op";
 
 export default {
   operations,
   requests,
   docNode,
-  urql,
-  vueUrql,
-  vueApollo,
-  vueApolloSmartOp,
+  // urql,
+  // vueUrql,
+  // vueApollo,
+  // vueApolloSmartOp,
 };
