2e0ccefcf19fc413ec8d3f572b9c20be7a6b58d4
Huakun Shen <huakun.shen@huakunshen.com>
2022-06-26 04:55:35 -0400

    The second version design works now on backend.
    Version is updated to 0.1.0-pre-alpha.6

---
+++ exp.ts
@@ -0,0 +1,8 @@
+import { OperationResult } from "urql";
+import  { types } from "./index";
+
+const x: types.DeleteRecordResponse = {
+  message: "",
+  success: true,
+};
+
--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
-import * as operations from "./generated/ops";
-import * as requests from "./generated/gql-req";
-import * as docNode from "./generated/doc-node";
+export * as operations from "./generated/ops";
+export * as requests from "./generated/gql-req";
+export * as docNode from "./generated/doc-node";
+export * as resolver from "./generated/resolver";
 // import * as urql from "./generated/urql";
 // import * as vueUrql from "./generated/vue-urql";
 // import * as vueApollo from "./generated/vue-apollo";
 // import * as vueApolloSmartOp from "./generated/vue-apollo-smart-op";
 
-export default {
-  operations,
-  requests,
-  docNode,
-  // urql,
-  // vueUrql,
-  // vueApollo,
-  // vueApolloSmartOp,
-};
+// export default {
+// operations,
+// requests,
+// docNode,
+// urql,
+// vueUrql,
+// vueApollo,
+// vueApolloSmartOp,
+// };
